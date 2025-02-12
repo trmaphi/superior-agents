af4e0f75f7eadbf5721d31794534d9bd34c39dac
let-me-cook <itsalfath123@gmail.com>
2025-02-12 10:55:02 +0000

    chore: defaults

--- agent/src/constant.py
+++ agent/src/constant.py
@@ -1,21 +1,21 @@
 FE_DATA_MARKETING_DEFAULTS = {
-	"agent_id": "testing_agent",
+	"agent_id": "testing_marketing_agent",
 	"model": "deepseek_2",
 	"role": "degen shitcoin-maxxing",
 	"time": "1h",
 	"metric_name": "followers",
 	"research_tools": ["CoinGecko", "DuckDuckGo", "Etherscan", "Infura"],
 	"prompts": {},
-	"trading_instruments": ["spot"],
 }
 
 FE_DATA_TRADING_DEFAULTS = {
-	"agent_id": "testing_agent",
+	"agent_id": "testing_trading_agent",
 	"model": "deepseek_2",
 	"role": "degen shitcoin-maxxing",
 	"time": "1h",
 	"metric_name": "wallet",
 	"research_tools": ["CoinGecko", "DuckDuckGo", "Etherscan", "Infura"],
 	"prompts": {},
+	"trading_instruments": ["spot"],
 }
 
